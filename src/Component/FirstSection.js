a8d1bad5790607a15cb6a39b08d90f8ac24b18fe
parvez13913 <parvezz13913@gmail.com>
2022-06-23 02:50:31 -0700

    Second Section design complite

--- src/Component/FirstSection.js
+++ src/Component/FirstSection.js
@@ -5,19 +5,19 @@ const FirstSection = () => {
     return (
         <div className='w-50'>
             <div class="d-flex">
-                <div className='box d-flex align-items-center justify-content-center' draggable="true">
+                <div className='box shadow d-flex align-items-center justify-content-center' draggable="true">
                     <span className='h5'>A</span>
                 </div>
-                <div className='box d-flex align-items-center justify-content-center ' draggable="true">
+                <div className='box shadow d-flex align-items-center justify-content-center ' draggable="true">
                     <span className='h5'>B</span>
                 </div>
-                <div className='box d-flex align-items-center justify-content-center ' draggable="true">
+                <div className='box shadow d-flex align-items-center justify-content-center ' draggable="true">
                     <span className='h5'>C</span>
                 </div>
-                <div className='box d-flex align-items-center justify-content-center ' draggable="true">
+                <div className='box shadow d-flex align-items-center justify-content-center ' draggable="true">
                     <span className='h5'>D</span>
                 </div>
-                <div className='box d-flex align-items-center justify-content-center ' draggable="true">
+                <div className='box shadow d-flex align-items-center justify-content-center ' draggable="true">
                     <span className='h5'>E</span>
                 </div>
             </div>
--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import FirstSection from './FirstSection';
 import './HomePage.css';
+import SecondSection from './SecondSection';
 
 const HomePage = () => {
     return (
         <div>
             <FirstSection />
+            <SecondSection />
         </div>
     );
 };
