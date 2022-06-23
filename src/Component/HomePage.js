b2c19c5025d97c23b3cbe3f98b6b0e641832074a
parvez13913 <parvezz13913@gmail.com>
2022-06-23 03:30:16 -0700

    Third Section Design almost Complite

--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import FirstSection from './FirstSection';
 import './HomePage.css';
 import SecondSection from './SecondSection';
+import ThirdSection from './ThirdSection';
 
 const HomePage = () => {
     return (
         <div>
             <FirstSection />
             <SecondSection />
+            <ThirdSection />
         </div>
     );
 };
